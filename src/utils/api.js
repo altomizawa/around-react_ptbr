b1b92dcb87e1789f5e096505b1fdfde5b66788f0
altomizawa <alyssontomizawa@hotmail.com>
2023-08-16 11:13:01 -0300

    export instance of Api class '

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import { apiUrl, authorization } from "../components/constants";
 
-export default class Api {
+class Api {
   constructor(url = apiUrl, authorizationToken = authorization) {
     this._url = url;
     this._authorization = authorizationToken;
@@ -99,3 +99,6 @@ export default class Api {
       });
   }
 }
+
+const newApi = Api();
+export default newApi;
